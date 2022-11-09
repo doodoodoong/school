1ada9d824d0a3309af31e6f17bcdea98c7fc54aa
doodoodoong <doo_doong@hanmail.net>
2022-11-09 09:15:05 +0900

    #

--- english_test.py
+++ english_test.py
@@ -12,6 +12,18 @@ word_list_10 = {
     "will": "~할 것이다",
 }
 
+word_list_11 = {
+    "get up": "일어나다",
+    "go to bed": "잠자리에 들다",
+    "read": "읽다",
+    "ride": "타다",
+    "drink": "마시다",
+    "exercise": "운동하다",
+    "every": "모든",
+    "day": "날,하루,요일",
+    "early": "일찍",
+}
+
 
 def lesson_10():
     word = word_list_10.keys()
@@ -23,4 +35,15 @@ def lesson_10():
     return rd_word, meaning
 
 
+def lesson_11():
+    word = word_list_11.keys()
+    rd_word = random.sample(word, 3)
+    meaning = []
+    for i in range(3):
+        mean = word_list_11[rd_word[i]]
+        meaning.append(mean)
+    return rd_word, meaning
+
+
 print(lesson_10())
+print(lesson_11())
